decd0b35a8b70a47d2f14f085c933c8812186f9f
Dezarea Bryan <dezareabryan@gmail.com>
2023-01-29 00:08:44 -0500

    styling && readme

--- src/components/LocationKeyCall.js
+++ src/components/LocationKeyCall.js
@@ -4,12 +4,14 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import CardGroup from "react-bootstrap/CardGroup";
 import WeatherForecast from "./WeatherForecast";
+import Card from "react-bootstrap/Card";
 
 const LocationKeyCall = () => {
-  const apiKey = process.env.REACT_APP_APIKEY
+  const apiKey = process.env.REACT_APP_APIKEY;
   const [zip, setZip] = useState("");
   const [locationKey, setLocationKey] = useState("");
   const [disabled, setDisabled] = useState(true);
+  const [hiddenHome, setHiddenHome] = useState(false)
 
   const fetchWeatherLocation = async () => {
     const getWeatherData = await fetch(
@@ -33,6 +35,7 @@ const LocationKeyCall = () => {
   const handleClick = () => {
     fetchWeatherLocation();
     setDisabled(false);
+    setHiddenHome(true)
   };
 
   return (
@@ -62,6 +65,16 @@ const LocationKeyCall = () => {
       <CardGroup hidden={disabled} className="mx-auto">
         <WeatherForecast locationKey={locationKey} />
       </CardGroup>
+      
+      <CardGroup id="weather-description-home" hidden={hiddenHome}>
+          <Card.Title id="dezsays-weather-app-card-title" className="text-white">DezSays Weather App</Card.Title>
+          <Card.Text id="dezsays-weather-app-card-text" className="text-white">
+            <b>
+            Welcome to DezSays Weather App! Enter your zip code in the search bar above, and search for the weather forecast in your area. You may select either the current weather forecast or the five day forecast for the given zip code. This application is available thanks to the free Accuweather API. 
+            </b>
+          </Card.Text>
+      </CardGroup>
+        
     </>
   );
 };
